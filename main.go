4fc605db339eed6a3e1a4109c336271e00281b7f
Jacob Gundlach <jacobgundlach13@outlook.com>
2024-06-14 15:37:00 -0400

    Added simple ai

--- main.go
+++ main.go
@@ -211,8 +211,101 @@ func playerLoop() {
 	}
 }
 
+func aiMove(a map[string]string) string {
+  if _, ok := a["f"]; ok {
+    return "f"
+  } else {
+    randNum := rand.IntN(2)
+    if randNum == 0 {
+      return "m"
+    } else {
+      return "s"
+    }
+  }
+}
+
 func aiLoop()  {
-  
+  fmt.Println("You are trapped in the dungeon.")
+	fmt.Println("Collect treasure and try to escape.")
+	fmt.Println("To play, type one of the command choices on each turn.")
+	fmt.Println()
+
+	actions := map[string]string{
+		"m": "move",
+		"s": "search",
+	}
+	// Main game loop
+	for healthPoints > 0 && !escaped {
+
+		fmt.Printf("Room number %d\n", roomsExplored)
+		fmt.Println(currentRoom)
+
+		fmt.Println()
+
+		if monster {
+			fmt.Println("OH NO! An evil monster is in here with you!")
+			actions["f"] = "fight"
+		} else {
+			delete(actions, "f")
+		}
+		fmt.Println()
+
+		for k, v := range actions {
+			fmt.Printf("%s - %s\n", k, v)
+		}
+
+		fmt.Println("What do you do?")
+
+    input := aiMove(actions)
+
+		fmt.Println()
+
+		if monster && monsterAttack() {
+			healthPoints -= 1
+			fmt.Println("OUCH! The monster hit you!")
+		}
+
+		switch input {
+		case "m":
+			currentRoom = createRoom()
+			roomsExplored += 1
+			monster = hasMonster()
+			escaped = hasEscaped()
+		case "s":
+			if hasTreasure() {
+				fmt.Printf("You found %s!\n", treasure())
+				treasureCount += 1
+			} else {
+				fmt.Println("You look, but don't find anything.")
+			}
+
+			if !monster {
+				monster = hasMonster()
+			}
+		case "f":
+			if defeatMonster() {
+				monster = false
+				fmt.Println("You defeated the monster!")
+			} else {
+				fmt.Println("You attack and miss!")
+			}
+		default:
+			fmt.Println("I don't know ho to do that!")
+			fmt.Println()
+		}
+
+		fmt.Println()
+	}
+
+	if healthPoints > 0 {
+		fmt.Printf("You escaped!\n")
+		fmt.Printf("You explored %d rooms.\n", roomsExplored)
+		fmt.Printf("You found %d treasures.\n", treasureCount)
+	} else {
+		fmt.Printf("OH NO! You didn't make it out.\n")
+		fmt.Printf("You explored %d rooms before meeting your doom.\n", roomsExplored)
+	}
+ 
 }
 
 func main() {
